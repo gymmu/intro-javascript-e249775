61f6f35389dd50a9a082e4d2a8af9d1f9e103420
Lucy Wehrli <e249775@sbl.ch>
2023-10-17 08:43:43 +0200

    increse decrese

--- script.js
+++ script.js
@@ -9,6 +9,34 @@ function changeBackground() {
 
 
 function deleteText() {
- 
+  const elemToDeleteText = document.querySelector("#to-delete")
+  elemToDeleteText.textContent = "Inhalt gelöscht"
 }
 
+function incrementCounter() {
+  const numberElement = document.querySelector("#counter")
+  let num = parseInt(numberElement.textContent)
+  num = num -1
+  numberElement.textContent = num
+}
+
+function incrementCounter() {
+  const numberElement = document.querySelector("#counter")
+  let num = parseInt(numberElement.textContent)
+  num = num +1
+  numberElement.textContent = num
+}
+
+function decrementCounter() {
+  const numberElement = document.querySelector("#counter")
+  let num = parseInt(numberElement.textContent)
+  num = num +1
+  numberElement.textContent = num
+}
+
+function decrementCounter() {
+  const numberElement = document.querySelector("#counter")
+  let num = parseInt(numberElement.textContent)
+  num = num -1
+  numberElement.textContent = num
+}
